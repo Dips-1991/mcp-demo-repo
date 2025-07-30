70bc47c8cdc6044fb04025ae7ca648c10b0c1969
Deepak Patil <patildipak0791@gmail.com>
2025-07-30 23:38:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import flask 
+import 
+
+import requests
+
+def main():
+    app = flask.Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello from pythonprograms!"
+
+    app.run()
+
+if __name__ == "__main__":
+    main()
